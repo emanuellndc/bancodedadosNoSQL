e45e1773ae71bb8711daeeeb8f06d6a340980998
emanuellndc <41972534+emanuellndc@users.noreply.github.com>
2018-08-01 10:29:09 -0300

    Recomendação de produtos utilizando Neo4j e Redis
    Recomendação de produtos utilizando Neo4j , Python e Redis.
    O Redis é um banco de dados não relacional com vantagens em escrita. Utilizado para carrinho de compras em lojas de e-commerce. votações em reality show ao vivo. O redis foi utilizado para simular das compras dos clientes.
    Para realizar a recomendação foi utilizado um outro banco de dados não relacional chamado : Neo4f. O Neo4j é um banco de dados orientado a grafos. O script python realiza a conexão com os bancos de dados e gera o grafo de compra de todos os clientes, em seguida ele realiza uma query "Quem comprou o produto A também comprou o produto B?" com a contagem de pessoas que comprou os dois produtos, recomenda-se o produto B para o cliente.
    O redis (chave e valor) e neo4j (orientado a grafos) são utilizados em Big Data.

---
+++ neo4jEmanuelLucas.py
@@ -0,0 +1,45 @@
+import redis
+from neo4jrestclient.client import GraphDatabase
+from neo4jrestclient import client
+r = redis.StrictRedis(host='localhost', port=6379, charset="utf-8", decode_responses=True, db=0)
+
+C1 = ['ProdutoA', 'ProdutoB','ProdutoC','ProdutoD']
+C2 = ['ProdutoA', 'ProdutoB']
+C3 = ['ProdutoA', 'ProdutoB','ProdutoC']
+C4 = ['ProdutoA', 'ProdutoB']
+
+r.set('C1', C1)
+r.set('C2', C2)
+r.set('C3', C3)
+r.set('C4', C4)
+
+chaves = r.keys()
+
+
+db = GraphDatabase("http://localhost:7474", username="neo4j", password="1234")
+cliente = db.labels.create("Clientes")
+produtosCliente = db.labels.create("ProdutosClientes")
+
+for i in chaves:
+   produtos = r.get(i)
+   a = produtos[1:-1]
+   a = a.split("', '")
+   a[0] = a[0][1:]
+   a[-1] = a[-1][:-1]
+   print(type(a))
+   c = db.nodes.create(name=i) 
+   cliente.add(c)
+   for j in a:
+             print("lista produtos "+j)
+             q = 'Merge(C:ProdutosClientes{name:"'+j+'"})'
+             db.query(q)
+             t = 'MATCH(C:Clientes),(P:ProdutosClientes) WHERE C.name="'+i+'"AND P.name ="'+j+'"CREATE (C)-[R:comprou]->(P) RETURN C,R,P'
+             db.query(t)
+b = 'MATCH (u:Clientes)-[:comprou]->(c1:ProdutosClientes), (u)-[:comprou]->(c2:ProdutosClientes)WHERE c1.name = "ProdutoA" AND c2.name = "ProdutoB" RETURN u'
+results = db.query(b, returns=(client.Node))
+i=0
+for r in results:
+    i = i + 1
+porcentagem =str((i/4)*100)
+print(porcentagem + " das pessoas que comprou o produto A, também comparam o produto B")
+
